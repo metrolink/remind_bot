6f4decaa10e0368ab1b0ede53dc4fb1eeef3bf31
Tor <to.an@hotmail.no>
2023-07-04 16:17:09 +0200

    added experimental branch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('./test_config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
